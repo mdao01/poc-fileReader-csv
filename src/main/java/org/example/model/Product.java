644d90f7ffece2504aff66344197ab90cdbf819e
Marcos Danilo de Almeida Oliveira <md.almeidaoliveira@gmail.com>
2024-01-30 23:32:14 -0300

    Commit Teste

--- src/main/java/org/example/model/Product.java
+++ src/main/java/org/example/model/Product.java
@@ -1,7 +1,6 @@
 package org.example.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +9,6 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Product {
 
     private String name;
